e285ae7240724fbad75fbc963b285ed1ac39f3b8
Ali Afify <ali.afify93746@gmail.com>
2022-10-29 16:36:57 +0200

    first commits

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const { main } = require("./mongoose.js");
 const routes1 = require("./Routes.js");
 const routes2 = require("./account2Routes.js");
-const routes3 = require("./account3Routes")
+const routes3 = require("./account3Routes");
 var passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
@@ -12,7 +12,7 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 // -----------End Of Imports
- 
+
 // --------------connect to mongoDB--------------
 mongoose.set("useCreateIndex", true); //for warning
 main().catch((err) => console.log(err));
@@ -23,8 +23,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", routes1);
 app.use("/api", routes2);
-app.use("/api",routes3)
-app.use(express.static('build'))
+app.use("/api", routes3);
+app.use(express.static("build"));
+app.use(express.static(path.resolve(__dirname, "build")));
 
 //-----------------Middleware------------------
 
